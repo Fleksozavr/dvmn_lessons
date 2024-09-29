a5ce7d3b85dc3dda2e0d758ecb07154b0cd3eb47
Fleksozavr <131782632+Fleksozavr@users.noreply.github.com>
2024-09-29 18:30:26 +0500

    add datetime fields to PokemonEntity

--- djangoorm/djangoorm4/pokemon_map/pokemon_entities/models.py
+++ djangoorm/djangoorm4/pokemon_map/pokemon_entities/models.py
@@ -13,4 +13,7 @@ class PokemonEntity(models.Model):
     lat = models.FloatField()
     lon = models.FloatField()
 
+    appeared_at = models.DateTimeField()
+    disappeared_at = models.DateTimeField()
+
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE) 
